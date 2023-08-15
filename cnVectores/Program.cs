a66159812a9d8e798b2297a08d68468021aa70de
Emmanuel <emmanuel.cartagena5980@unaula.edu.co>
2023-08-14 23:20:58 -0500

    Agregar archivos de proyecto.

---
+++ cnVectores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void PoblarVector(int[] vector)
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < vector.Length; i++)
+        {
+
+            vector[i] = random.Next(1, 101);
+            Console.WriteLine($"Poblar con: " + vector[i]);
+        }
+    }
+    static void ImprimirVector(int[] vector)
+    {
+        Console.WriteLine("Imprimir");
+
+        for (int i = 0; i < vector.Length; i++)
+        {
+            Console.WriteLine($"Posición {i + 1}: {vector[i]}");
+        }
+    }
+    static void Main(string[] args)
+    {
+        int[] vector1 = new int[6];
+        PoblarVector(vector1);
+        ImprimirVector(vector1);
+    }
+}
